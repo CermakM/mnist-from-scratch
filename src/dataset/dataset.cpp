fb0fe0bd346a7d09bab08cc0d94c2c85166b6777
Marek Cermak <macermak@redhat.com>
2018-12-16 15:56:46 +0100

    Add endianity check when loading MNIST data
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   src/dataset/dataset.cpp

--- src/dataset/dataset.cpp
+++ src/dataset/dataset.cpp
@@ -143,9 +143,11 @@ std::vector<T> images::mnist::read_data_file(const std::string &fpath, const siz
     in_file.read(reinterpret_cast<char*> (&magic_number), sizeof(magic_number));
     in_file.read(reinterpret_cast<char*> (&n_images), sizeof(n_images));
 
-    // swap endianity
-    magic_number = __bswap_32(magic_number);
-    n_images = __bswap_32(n_images);
+    if (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__) {
+        // swap endianity
+        magic_number = __bswap_32(magic_number);
+        n_images = __bswap_32(n_images);
+    }
 
     if (magic_number != 2049 && magic_number != 2051) {
         throw std::runtime_error("Magic number values do not match.");
